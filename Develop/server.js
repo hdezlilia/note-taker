480b7a456572c82b9f4cf32ac5f56ef3c24f1c5f
hdezlilia <127769063+hdezlilia@users.noreply.github.com>
2023-09-06 21:41:20 -0700

    removing files

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs'); 
 // generated unique ID's
-const {v4: uuidv4} = required('uuid');
+const {v4: uuidv4} = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
